6880fb971a49872e532815138870740092b84342
Mikhail Kruk <mi.kruk95@gmail.com>
2026-05-23 23:37:02 +0200

    test: add unit tests for catalogBatchProcess lambda

---
+++ product_service/test/mocks/products.ts
@@ -0,0 +1,18 @@
+
+const mockProduct = [
+	{
+		id: '1',
+		title: 'AK-47',
+		description: 'Skin',
+		price: 10,
+	},
+];
+
+const mockStock = [
+	{
+		product_id: '1',
+		count: 10,
+	},
+];
+
+export { mockProduct, mockStock };
